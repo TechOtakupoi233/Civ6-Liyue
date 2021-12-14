f275626b7cc850de736e92f9795f4fcbb9b37766
TechOtakupoi233 <slkj104@gmail.com>
2021-12-14 18:47:51 +0800

    Update Keqing_Builders.lua

--- Liyue/Liyue/Data/Keqing_Builders.lua
+++ Liyue/Liyue/Data/Keqing_Builders.lua
@@ -1,10 +1,37 @@
-function OnTrainingComplete(playerID, cityID, iConstructionType, unitID, bCancelled)
-    local pPlayer = Players[iplayerID];
-    local unit = GameInfo.Units["UNIT_BUILDER"];
-    if (iConstructionType ~= unit) then
-        pUnitManager.Kill(unitID);
-    end;
+function OnTurnStarted(playerID)
+    if HasTrait("TRAIT_LEADER_THE_STAR_OF_CHANGE", playerID) then -- short-circuit when player has no trait
+        local pPlayer = Players[playerID];
+        local pUnit = pPlayer:GetUnits():FindID(unitID);
+        if pUnit ~= nil then
+            local unitType = GameInfo.Units[pUnit:GetType()].UnitType;
+            if unitType == "UNIT_BUILDER" then
+                local pUnitAbility = pUnit:GetAbility();
+                if HasAbility(playerID, unitID, "ABILITY_KEQING_EXTENDED_SHIFT") then
+				    UnitManager.RestoreMovement(pUnit);
+					UnitManager.RestoreUnitAttacks(pUnit);
+                end
+            end
+        end
+    end
 
 end
 
-Events.CityProductionCompleted.Add(OnTrainingComplete)
+function OnUnitChargesChanged_ExtendedShift(playerID, unitID, newCharges, oldCharges)
+    if HasTrait("TRAIT_LEADER_THE_STAR_OF_CHANGE", playerID) then -- short-circuit when player has no trait
+        local pPlayer = Players[playerID];
+        local pUnit = pPlayer:GetUnits():FindID(unitID);
+        if pUnit ~= nil then
+            local unitType = GameInfo.Units[pUnit:GetType()].UnitType;
+            if unitType == "UNIT_BUILDER" then
+                local pUnitAbility = pUnit:GetAbility();
+                if HasAbility(playerID, unitID, "ABILITY_KEQING_EXTENDED_SHIFT") then
+				    UnitManager.RestoreMovement(pUnit);
+					UnitManager.RestoreUnitAttacks(pUnit);
+                end
+            end
+        end
+    end
+end
+
+Events.PlayerTurnStarted.Add(OnTurnStarted)
+Events.UnitChargesChanged.Add(OnUnitChargesChanged_ExtendedShift);
