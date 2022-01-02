cd2149bb09f5f8c0150484345b42e0be54f7554d
dwughjsd <94778865+dwughjsd@users.noreply.github.com>
2022-01-02 13:00:54 +0800

    Civilopedia

--- Liyue/Liyue/Text/Liyue_SharedText.sql
+++ Liyue/Liyue/Text/Liyue_SharedText.sql
@@ -6,9 +6,12 @@ INSERT OR IGNORE INTO LocalizedText (Language, Tag, Text)		-- Names Included in 
 VALUES	('en_US',	'LOC_BUILDING_STATUE_OF_THE_SEVEN_NAME',			'Statue of the Seven'),
 		('en_US',	'LOC_BUILDING_STATUE_OF_THE_SEVEN_DESCRIPTION',		'Genshin Impact unique building that replaces the Monument. Provides an additional +2 [ICON_Faith] Faith. Provides +1 Loyalty per turn in this city. If the city already has maximum Loyalty, also provides an additional +1 [ICON_Culture] Culture.[NEWLINE]Units instantly heals when moved to a Statue of the Seven. The healing power of the statue is 25 per turn.'),
 		('en_US',	'LOC_TOOLTIP_STATUE_OF_THE_SEVEN',					"Statue's Blessing: "),
+		('en_US',	'LOC_PEDIA_BUILDINGS_PAGE_BUILDING_STATUE_OF_THE_SEVEN_CHAPTER_HISTORY_PARA_1',					''	),
+
 
 		('en_US',	'LOC_IMPROVEMENT_ADVENTURERS_GUILD_NAME',			"Adventurers' Guild"),
 		('en_US',	'LOC_IMPROVEMENT_ADVENTURERS_GUILD_DESCRIPTION',	"Unlocks the Builder ability to constrcut a Adventurers' Guild, unique to Genshin Impact.[NEWLINE][NEWLINE]+1 [ICON_Culture] Culture and [ICON_Production] Production. +1 [ICON_Gold] Gold and Appeal to adjacent tiles. One per city. Tiles with Adventurers' Guild cannot be swapped."),
+		('en_US',	'LOC_PEDIA_IMPROVEMENTS_PAGE_IMPROVEMENT_ADVENTURERS_GUILD_CHAPTER_HISTORY_PARA_1',					''	),
 
 		('en_US',	'LOC_UNIT_HILICHURL_FIGHTER_NAME',	'Hilichurl'	),
 		('en_US',	'LOC_UNIT_HILICHURL_GUARD_NAME',	'Mitachurl'	),
@@ -24,9 +27,12 @@ VALUES	('en_US',	'LOC_BUILDING_STATUE_OF_THE_SEVEN_NAME',			'Statue of the Seven
 		('zh_Hans_CN',	'LOC_BUILDING_STATUE_OF_THE_SEVEN_NAME',			'七天神像'		),
 		('zh_Hans_CN',	'LOC_BUILDING_STATUE_OF_THE_SEVEN_DESCRIPTION',		'替代纪念碑的原神特色建筑。额外提供 +2 [ICON_Faith] 信仰值。使此城市每回合 +1 忠诚度。如城市已拥有最高忠诚度，则额外 +1 [ICON_Culture] 文化值。[NEWLINE]移动至七天神像的单位立即回复生命值（每回合最多25点）。'),
 		('zh_Hans_CN',	'LOC_TOOLTIP_STATUE_OF_THE_SEVEN',					'神像的恩泽：'	),
+		('zh_Hans_CN',	'LOC_PEDIA_BUILDINGS_PAGE_BUILDING_STATUE_OF_THE_SEVEN_CHAPTER_HISTORY_PARA_1',					'七天神像是镇驻在提瓦特各处的巨大石像，据说它们的形象来自于七位神明。它们可以在提瓦特大陆的各处找到，七国中每个国家的神像都与它们的神所对应，分别诉说着各自的长久历史——在蒙德有5座，璃月有5座，稻妻则有6座。一开始，旅行者只能点亮位于坠星山谷和风啸山坡的七天神像，而在可探索的区域扩展之后则可以前往其他神像处。当旅行者首次点亮一处七天神像时，可以获得额外的视野和新的探索派遣。它还可以切换旅行者的共鸣状态，并为旅行者提供治疗的恩泽。在雪山的七天神像附近时，还可以降低角色的寒冷值。'	),
+
 
 		('zh_Hans_CN',	'LOC_IMPROVEMENT_ADVENTURERS_GUILD_NAME',			'冒险家协会'		),
 		('zh_Hans_CN',	'LOC_IMPROVEMENT_ADVENTURERS_GUILD_DESCRIPTION',	'解锁建造冒险家协会（原神特色改良设施）的能力。[NEWLINE][NEWLINE]+1 [ICON_Culture] 文化值 +1 [ICON_Production] 生产力，为相邻单元格提供 +1 [ICON_Gold] 金币 +1 魅力。每个城市仅限建造一座。冒险家协会所在的单元格无法被切换给另一座城市。'),
+		('zh_Hans_CN',	'LOC_PEDIA_IMPROVEMENTS_PAGE_IMPROVEMENT_ADVENTURERS_GUILD_CHAPTER_HISTORY_PARA_1',					'冒险家协会是活跃于提瓦特七国的组织，为私人和组织提供私募冒险家服务。它是帮助新人冒险者适应冒险生活的平台，也为老手提供便利的帮助。冒险家协会收集来自各处口耳相传的情报，并将其编制为委托和冒险并安排给冒险者。有关冒险家协会的起源人们知之甚少，不过可以查知的是，这一协会的总部位于至冬国。冒险家协会的格言是“Ad Astra Abyssosque”（向着星辰与深渊），不过甚至是寻找失踪的猫咪这样的工作也在它们的职责范围内。'	),
 
 		('zh_Hans_CN',	'LOC_UNIT_HILICHURL_FIGHTER_NAME',	'丘丘人'		),
 		('zh_Hans_CN',	'LOC_UNIT_HILICHURL_GUARD_NAME',	'丘丘人首领'	),
